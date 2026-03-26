435a25a00cc21aeeac062e88d98fe2e3076093c1
hflocki <heiko.flocke@gmail.com>
2026-03-26 09:10:38 +0100

    Update binary_sensor.py
    change sensor wording to moving

--- custom_components/geoweather/binary_sensor.py
+++ custom_components/geoweather/binary_sensor.py
@@ -45,7 +45,8 @@ class GeoWeatherMovingBinarySensor(BinarySensorEntity):
     def __init__(self, coordinator: GeoWeatherCoordinator, entry: ConfigEntry) -> None:
         self._coordinator = coordinator
         self._entry = entry
-        self._attr_name = "Fährt"
+        # Wir ändern den internen Namen auf 'Moving'
+        self._attr_name = "Moving" 
         self._attr_unique_id = f"{entry.entry_id}_moving"
 
     @property
